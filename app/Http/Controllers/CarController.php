b63b1221bd87c83ad358cbfdc7c09b354d249869
jeffChan1993 <ccyuk93@gmail.com>
2022-01-23 23:34:40 -0500

    add demo create / delete / update

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -44,31 +44,57 @@ class CarController extends BaseController
 
     }
 
-    function store(Request $request){
+    /**
+     *  if we make sure all the crate / update / delete function of different Controller is the same
+     *  We can write it into a new Controller and extends this Controller
+     *  For example
+     *  defines =>public $model_class = Car::class in CarController.php
+     *  then =>$this->model_class::find(1) to get the Car Model result in Controller.php
+     */
+
+
+//    function store(Request $request){
 //    try {
-//        $cars = Car::create($request->all);
+//        $cars = Car::create([
+//            'house_id'=>$request->get('house_id'),
+//            'license_plate'=>$request->get('license_plate'),
+//            'brand'=>$request->get('brand'),
+//            'color'=>$request->get('color'),
+//          ]);
 //        return response()->json(['data'=>$cars],200);
 //    }catch (\Exception $e){
 //            return response()->json(['error'=>$e->getMessage()],500);
 //        }
-    }
+//    }
 
-    function update(Request $request){
+//    function update(Request $request){
 //    try {
-//        $cars = Car::update($request->all);
+//        $cars = Car::update([
+//            'house_id'=>$request->get('house_id'),
+//            'license_plate'=>$request->get('license_plate'),
+//            'brand'=>$request->get('brand'),
+//            'color'=>$request->get('color'),
+//            ]);
 //        return response()->json(['data'=>$cars],200);
 //    }catch (\Exception $e){
 //            return response()->json(['error'=>$e->getMessage()],500);
 //        }
-    }
+//    }
 
-    function delete(Request $request){
+//    function delete(Request $request){
 //        try {
-//        $cars = Car::destory($request->id);
-//        return response()->json(['data'=>$cars],200);
+//
+//            $result = null;
+//
+//            $car = Car::find($request->get('id'));
+//            $result = $car->delete();
+//
+//            // or you can ues destory
+//             $result = Car::destory($request->get('id'));
+//        return response()->json(['data'=>$result],200);
 //        }catch (\Exception $e){
 //            return response()->json(['error'=>$e->getMessage()],500);
 //        }
 
-    }
+//    }
 }
